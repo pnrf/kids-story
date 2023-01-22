d37ae72ce7045a482d585df6791ad914fe31be71
Pankratov <pnrf@yandex.ru>
2023-01-22 20:41:37 +0300

    feat: add section Team

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,43 +5,64 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 // ------------ swiper ------------
-const slider = document.querySelector('.swiper');
-const videoGallery = document.querySelector('.slider-video');
+// const slider = document.querySelector('.swiper');
+// const videoGallery = document.querySelector('.slider-video');
+// const sliderTeamClass = document.querySelector('.slider-team');
 
-const swiper = new Swiper(slider, {
+// const swiper = new Swiper(slider, {
+//   modules: [Navigation, Pagination],
+
+//   direction: 'horizontal',
+//   loop: true,
+
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+//   navigation: {
+    // nextEl: '.slider__button-next',
+    // prevEl: '.slider__button-prev',
+//   },
+
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+// });
+
+// const sliderVideo = new Swiper(videoGallery, {
+// 	slidesPerView: 3,
+// 	spaceBetween: 10,
+// 	centeredSlides: true,
+
+//   modules: [Pagination],
+
+//   direction: 'horizontal',
+//   loop: true,
+
+//   pagination: {
+//     el: '.swiper-pag',
+//     type: 'bullets',
+//     clickable: true,
+//   },
+// });
+
+const swiperSectionTeam = new Swiper(".swiper-section-team", {
   modules: [Navigation, Pagination],
-
-  direction: 'horizontal',
+  slidesPerView: 4,
+	spaceBetween: 32,
+  slidesPerGroup: 4,
   loop: true,
+  loopFillGroupWithBlank: true,
 
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
+    type: 'bullets',
+    clickable: true,
   },
 
   navigation: {
-    nextEl: '.slider__button-next',
-    prevEl: '.slider__button-prev',
-  },
-
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
-
-const sliderVideo = new Swiper(videoGallery, {
-	slidesPerView: 3,
-	spaceBetween: 10,
-	centeredSlides: true,
-
-  modules: [Pagination],
-
-  direction: 'horizontal',
-  loop: true,
-
-  pagination: {
-    el: '.swiper-pag',
-    type: 'bullets',
-    clickable: true,
+    nextEl: '.slider__button-next_section_team',
+    prevEl: '.slider__button-prev_section_team',
   },
 });
 
